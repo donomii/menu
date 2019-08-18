e38228cb8a77446089ac255847abbd290cdf7418
Jeremy Price <donomii@gmail.com>
2019-08-18 16:52:30 +0200

    Fix crash on stale pid file

--- helpers/command_line_toggle.go
+++ helpers/command_line_toggle.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"strconv"
-
 	//"time"
 
 	"github.com/donomii/goof"
@@ -19,12 +19,21 @@ func pidPath() string {
 
 func main() {
 	pidfile := pidPath()
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Recovered in f", r)
+			log.Println("Removing pid file to prevent locking user out of menu")
+			os.Remove(pidfile)
+		}
+	}()
 	launchDir := goof.ExecutablePath()
 	if goof.Exists(pidfile) {
 		pidString, _ := ioutil.ReadFile(pidfile)
 		pid, _ := strconv.Atoi(string(pidString))
-		proc, _ := os.FindProcess(pid)
-		proc.Kill()
+		proc, err := os.FindProcess(pid)
+		if err == nil {
+			proc.Kill()
+		}
 		os.Remove(pidfile)
 	} else {
 		cmd := exec.Command(launchDir + "/universal_menu_main.exe")
