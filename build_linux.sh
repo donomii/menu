d79bae736eb9c3d0c91282f08920f5ae20b7b097
Jeremy Price <jeremy.price@careem.com>
2019-09-08 22:53:37 +0200

    Add linux build script

---
+++ build_linux.sh
@@ -0,0 +1,4 @@
+mkdir build
+go build -o build/universal_menu_main.exe
+go build -o build/universal_menu_command_line_toggle helpers/command_line_toggle.go
+#FIXME compile touchbar/global hotkey program here
