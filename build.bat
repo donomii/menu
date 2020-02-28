3331eba124b416707d4b3fb263748cc74ccbf336
Jeremy Price <donomii@gmail.com>
2020-02-28 08:08:48 +0100

    Package for ms windows

--- build.bat
+++ build.bat
@@ -2,3 +2,4 @@ mkdir build
 go build -ldflags -H=windowsgui -o build\universal_menu_main.exe
 go build -o build\universal_menu_command_line_toggle.exe helpers\command_line_toggle.go
 gcc helpers\windowsGlobalKeyHook.c -obuild/universal_menu_hotkey_monitor.exe
+gcc helpers\hotcorner.c -obuild/universal_menu_hotcorner_monitor.exe
