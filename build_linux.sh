d1be5c06cbf38938ab844754fee3e1e721e7284d
donomii <donomii@gmail.com>
2021-04-28 07:49:42 +0200

    Fix linux build

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,6 @@
 sudo apt-get install -y libegl1-mesa-dev libgles2-mesa-dev libx11-dev libxrandr-dev libxinerama-dev  libxinerama1 libxcursor-dev libxi-dev xorg-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev
 mkdir build
-go build -o build/universal_menu_main.exe
+go build -o build/universal_menu_main.exe ./glfw/
 go build -o build/universal_menu_command_line_toggle helpers/command_line_toggle.go
+go build -o build/tray.exe ./tray/
 #FIXME compile touchbar/global hotkey program here
