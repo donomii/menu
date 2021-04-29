65daa6fb23576795f17d5f1fc7386334c445b694
donomii <donomii@gmail.com>
2021-04-29 23:07:29 +0200

    Add tiered applications menu

--- tray/main.go
+++ tray/main.go
@@ -278,7 +278,7 @@ func onReady() {
 	fmt.Printf("%+v, %v\n", m.SubNodes, m)
 	systray.AddMenuItem("UMH", "Universal Menu")
 
-	apps := menu.AppsMenu()
+	apps := menu.TieredAppsMenu()
 
 	m.SubNodes = append(m.SubNodes, apps)
 
